18a92046d4193165dd63b3f6d4d1577c9e1a7e80
Leonardo Barcaroli <leonardo.barcaroli@prima.it>
2019-12-17 23:49:12 +0100

    Add test on log config validation.

--- tests/test_conf.py
+++ tests/test_conf.py
@@ -89,3 +89,27 @@ def test_port_validation(port: int, tmpdir: py.path.local) -> None:
 
     assert "is invalid in configuration" in str(e.value)
     assert str(port) in str(e.value)
+
+
+@pytest.mark.parametrize(
+    "log_conf, msg",
+    [
+        [{"level": "WARN"}, "Allowed values are ('DEBUG', 'INFO', 'WARNING', 'ERROR')"],
+        [{"syslog": 1}, "Allowed values are ('true', 'false')"],
+        [
+            {"log_file": "/not/existing/path/to/logfile"},
+            "Base directory does not exist",
+        ],
+    ],
+)
+def test_log_validation(
+    log_conf: T.Dict[T.Text, T.Any], msg: T.Text, tmpdir: py.path.local
+) -> None:
+    """Test the conf validation"""
+    conf_path = fill_mock_conf({"log": log_conf}, tmpdir)
+    with pytest.raises(ValueError) as e:
+        with mock.patch("kofi.config._find_conf", return_value=str(conf_path)):
+            conf = read_config()
+
+    assert "is invalid in configuration" in str(e.value)
+    assert msg in str(e.value)
