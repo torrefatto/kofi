3482945f54f6f54624d7ce5d8ee1f8f947fe16f8
Leonardo Barcaroli <leonardo.barcaroli@prima.it>
2019-12-19 17:28:15 +0100

    Add interpolate REST endpoint.

--- kofi/rest.py
+++ kofi/rest.py
@@ -9,7 +9,7 @@ from codicefiscale import codicefiscale
 async def verify(req: web.Request) -> web.Response:
     """Validate and handle verify request."""
     cf = req.query.get("cf")
-    req.app["log"].info(f"Received request for cf -> {cf}")
+    req.app["log"].info(f"Received request for {cf}")
     if not cf:
         return web.json_response({"error": "malformed request", "cf": cf}, status=400)
     if codicefiscale.is_valid(cf):
@@ -20,4 +20,18 @@ async def verify(req: web.Request) -> web.Response:
 
 async def interpolate(req: web.Request) -> web.Response:
     """Validate and handle interpolate request."""
-    pass
+    name = req.query.get("name")
+    surname = req.query.get("surname")
+    gender = req.query.get("gender")
+    date_of_birth = req.query.get("date_of_birth")
+    place_of_birth = req.query.get("place_of_birth")
+    req.app["log"].info(
+        f"Received request for ({name}, {surname}, {gender}, {date_of_birth}, {place_of_birth})"
+    )
+    try:
+        cf = codicefiscale.encode(surname, name, gender, date_of_birth, place_of_birth)
+    except ValueError as e:
+        return web.json_response(
+            {"error": "malformed request", "error_msg": str(e)}, status=400,
+        )
+    return web.json_response({"result": cf})
